f3ab308bad49668d4b52c955f25267855a8b6aa4
georgesmbock <georgesmbo89@gmail.com>
2024-03-12 10:51:06 +0100

    su

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ import pandas as pd
 import numpy as np
 import requests as rq
 from bs4 import BeautifulSoup as bs
-print(f"{9 + 9}")
+print(f"{9 + 9 + 2}")
 print("Hello, World!")
 print("voir et merci")
