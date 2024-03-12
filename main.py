146e95827eb5981dded24d8eae901b9937996eab
georgesmbock <georgesmbo89@gmail.com>
2024-03-12 09:59:27 +0100

    modification de main

--- main.py
+++ main.py
@@ -3,20 +3,3 @@ import numpy as np
 import requests as rq
 from bs4 import BeautifulSoup as bs
 import time
-
-#soup =bs("", "html.parser")
-
-def parsers(e):
-    """"Cette fonction vérifie si un élément sélectionner dans soup exite puis
-    affiche contenu comme text; On utilise cette appelle cette fonction
-    dans la fonction simple_find crée en dessus.
-    
-    Args:
-        e (une balise): _description_
-
-    Returns:
-        _type_: _description_
-    """
-    if e:
-        return e.text
-    return None
